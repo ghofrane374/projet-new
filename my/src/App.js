811fabc4e4e18227f4a3461aad7550c545394e65
ghofrane <ghofrane.akrout@gmail.com>
2020-07-21 17:10:47 +0100

    mon commentaire

--- my/src/App.js
+++ my/src/App.js
@@ -1,13 +1,19 @@
 import React from "react";
-import "./App.css";
 import myBeauty from "./myBeauty.jpg";
+import "./App.css";
 
 function App() {
   return (
     <div>
       <div style={{ border: "30px solid black", maxWidth: "100vw" }}>
         <h1 className="title"> ghofrane akrout</h1> <br />
-        <image src={myBeauty} />
+        <img src={myBeauty} />
+        <img src="/fleur.jpg" />
+      </div>
+      <div>
+        <video width="320" height="240" controls>
+          <source src="myVideo.mp4" type="video/mp4" />
+        </video>
       </div>
     </div>
   );
